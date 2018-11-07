4be2f7bc152224cb3ec33ce14ca9984abd9b71da
leehoy <leehoy@kaist.ac.kr>
2018-11-07 16:48:47 +0900

    beams from multiple angles are handled

--- DoseCal_mono.m
+++ DoseCal_mono.m
@@ -2,6 +2,6 @@ LoadCoefficients;
 MakePhantom2;
 DefineBeam_2D;
 terma=terma_mono(fluence_1d,source_params,ph,phantom_params,attns,density);
-figure(2);imshow(terma,[]);
-figure(3);plot(terma(:,128));hold on;plot(ph(:,128));hold off;
+figure(2);imshow(sum(terma,3),[]);
+% figure(3);plot(terma(:,128));hold on;plot(ph(:,128));hold off;
 % beam_location=beam_path(beam_pos,beam_direction,beam_energy,ph,x,y,dx,dy);
--- terma_mono.m
+++ terma_mono.m
@@ -3,12 +3,26 @@ function [ terma ] = terma_mono(fluence,source_params,phantom,phantom_params...
 % Function to calculate total energy released per unit mass(TERMA)
 %   D=
 E=source_params.beam_energy;
-E_flu=E*fluence;
+% E_flu=E*fluence;
 [MassAttnMono,AttnMono]=GetAttenMono(phantom,E,attns,density);
 
 phantom_mask=ones(size(phantom));
 phantom_mask(phantom==0)=0;
-terma=RayTracing_2D(E_flu,source_params,AttnMono,MassAttnMono,phantom_params).*phantom_mask;
+beam_nx=source_params.beam_nx;
+beam_dx=source_params.beam_dx;
+beam_angles=source_params.beam_angles;
+beam_SAD=source_params.beam_SAD;
+terma=zeros([size(phantom),length(beam_angles)]);
+for i=1:length(beam_angles)
+    E_flu=E*fluence(:,i);
+    beam_angle=beam_angles(i);
+    beam_center=[beam_SAD*sind(beam_angle), beam_SAD*cosd(beam_angle)];
+%     source_params.beam_angle=beam_angle;
+    source_params.beam_center=[beam_SAD*sind(beam_angle), beam_SAD*cosd(beam_angle)];
+    source_params.beam_x=beam_center(1)+cosd(beam_angle)*((0:beam_nx-1)-(beam_nx-1)/2)*beam_dx;
+    source_params.beam_y=beam_center(2)-sind(beam_angle)*((0:beam_nx-1)-(beam_nx-1)/2)*beam_dx;
+    terma(:,:,i)=RayTracing_2D(E_flu,source_params,AttnMono,MassAttnMono,phantom_params).*phantom_mask;
+end
 % terma=zeros(size(phantom));
 % terma=
 end
